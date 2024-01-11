81e777b6c107a652bf79948120ffb7ad50010443
nahidhk <nahidappltd@gmail.com>
2024-01-11 16:41:05 +0600

    update

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log('Hello World!');
+// added a new nav system
+function  manu9(){
+document.getElementById("menu").classList="vv menu animate__animated animate__bounceInLeft"
+}
+function  clo(){
+    document.getElementById("menu").classList="menu animate__animated animate__bounceOutLeft"
+    }
+
