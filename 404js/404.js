04db97a588078c51d41c2cbdfba19e4aec1b683d
nahidappltd@gmail.com <nahidappltd@gmail.com>
2024-01-16 19:54:36 +0600

    new update

---
+++ 404js/404.js
@@ -0,0 +1,119 @@
+var for0for = `
+  <section class="nav">
+    <div class="nav-logo animate__animated  animate__rubberBand">
+      ReadyOfferCareer
+    </div>
+
+<div onclick="manu9()" class="menu-div">
+  <i class="bi bi-list"></i>
+</div>
+  </section>
+  <!-- new menu bar section -->
+ <section class="none" id="menu">
+  <div onclick="clo()" class="menu-div">
+    <i style="color: rgb(242, 255, 0);" class="bi bi-x-lg"></i>
+  </div>
+  <!-- the use a compit js onclick event -->
+  <div class="rxlink-box">
+   
+ <p onclick="window.location.href='https://smrturl.co/o/441952/1196338?s1=Walmart'">Walmart Gift
+Card $1000</p>
+<hr/>
+ <p onclick="window.location.href='https://smrturl.co/o/441952/53251150?s1=Paypal'">$1000 PayPal
+Gift Card</p>
+<hr/>
+ 
+<p onclick="window.location.href='https://smrturl.co/o/441952/53177516?s1=cashapp'"> $750 Cash App
+Gift Card</p>
+<hr/>
+ 
+ <p onclick="window.location.href='https://smrturl.co/o/441952/53265173?s1=Google'">Google Play
+Gift Card 1000$</p>
+ 
+ 
+ 
+</div>
+
+ </section>
+<br><br><br><br>
+  <!--/ new menu bar section/ -->
+
+  <section>
+    
+    <center>
+      
+    <p style="color: red;font-size: 100px;">404</p>
+  <p style="color: red;">404 Page not found ! Please try agin ! </p>
+ <a href="/index.html" class="btn">Go Back Home  </a>   
+          </center>
+
+  <br><br><br><br>
+  <br><br><br><br>
+<style>
+  .btn{
+    border:1px solid #4680ff;
+    padding: 10px;
+    border-radius: 10px;
+    color: #4680ff;
+    text-decoration: none;
+  }
+  .btn:hover{
+   background-color: #4680ff;
+   color: #fff;
+  }
+  
+</style>
+  
+  
+  
+  
+  
+  
+  
+    </section>
+
+  
+  
+ 
+
+
+
+<!-- foter down --->
+<section class="foter">
+  <br>
+  <div class="had-box">
+    <blockquote>
+      <span>About ReadyOfferCareer.com</span>
+  <p>ReadyOfferCareer.com is an affiliate website. Various Gift Cards, Make money, Cash App, Game Reward, phone reward, etc. are available here.</p>    
+    <p onclick="window.location.href='PrivacyPolicy'"  class="link"><i class="bi bi-shield-check"></i> Privacy Policy</p>
+      
+      
+      
+    </blockquote>
+  </div>
+  
+ <center>
+  <h1>ReadyOfferCareer</h1> 
+    <div class="aboust">
+
+<div><i class="bi bi-facebook"></i>
+</div>   
+<div><i class="bi bi-twitter-x"></i></div>   
+<div><i class="bi bi-instagram"></i></div>   
+<div><i class="bi bi-linkedin"></i></div>   
+<div onclick="window.location.href='https://github.com/nahidhk'"><i class="bi bi-github"></i></div>   
+ </center>
+  </div>
+  <center>
+    <blockquote>
+      
+    <p>Copyright &copy; 2024 - Ready Offer Career | Powered by<span class="link" onclick="window.location.href='https://www.nahidhk.info'">Nahid HK</span> </p>
+        </blockquote>
+
+  </center>
+  <br><br>
+</section>
+
+
+`
+document.getElementById("forr").innerHTML= for0for;
